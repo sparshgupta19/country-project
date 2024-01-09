a34631393e125665de7a73bfcfa753f140305a57
Sparsh <sparsh2000gupta@gmail.com>
2024-01-09 17:36:57 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const getCountriesData = async () => {
+    try {
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      const countries = await res.json();
+      setData(countries);
+    } catch (err) {
+      console.err(err);
+    }
+  };
+  useEffect(() => {
+    getCountriesData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containerStyle">
+        {data.map((item) => {
+          return (
+            <div className="cardStyle">
+              <img
+                src={item.flags.png}
+                alt="country-flag"
+                width={100}
+                height={100}
+              />
+              <h2>{item.name.common}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
